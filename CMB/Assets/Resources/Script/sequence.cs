625b1c6f31c7dc6d3ca7c6c8d337c4e448dd0f4f
DawnOfHope <DawnOfHope.zora@gmail.com>
2017-01-09 21:44:15 +0800

    月份背景自動排序

---
+++ CMB/Assets/Resources/Script/sequence.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sequence : MonoBehaviour {
+	
+
+	// Use this for initialization
+	void Awake(){
+		
+		for (int i = 0; i < 10; i++) {
+			automaticSorting();
+		}
+	}
+	public void automaticSorting(){
+		GameObject a1 = Instantiate (Resources.Load ("ResDATA/Month1")) as GameObject;
+		a1.transform.SetParent (transform, false);
+		
+	}
+}
